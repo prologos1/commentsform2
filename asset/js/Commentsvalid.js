32285601ae929d11d31a32518b06ffb64ac2bce4
prologos1 <101469742+prologos1@users.noreply.github.com>
2022-04-14 01:29:16 +0300

    Update Commentsvalid.js

--- asset/js/Commentsvalid.js
+++ asset/js/Commentsvalid.js
@@ -8,12 +8,12 @@ class Commentsvalid {
   isValidMessage()  {
     return (this.message.length > 4) ? true : false;
   }
-  isEmptyName()  {
+  isNotEmptyName()  {
     return (this.name.length > 2) ? true : false;
   }
   
-  isValidEmailAddress(emailAddress) {
-	const pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z\s]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
-	return pattern.test(emailAddress);
-	}
+  isValidEmail() {
+	const pattern = /\S+@\S+\.\S+/;
+	return pattern.test(this.email);
+  }
 }
